63e65f7fbe957bc5cfd4ff24b6c079bbaee7dc8f
nqdinh-1203 <quangdinhnt1203@gmail.com>
2022-04-20 19:03:05 +0700

    dbs relationships

--- midterm/accounts/models.py
+++ midterm/accounts/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
@@ -12,6 +10,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+        
 
 class Product(models.Model):
     CATEGORY = (
@@ -23,6 +27,10 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True,choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateField(auto_now_add=True, null=True)
+    tag = models.ManyToManyField(Tag, null=True)
+    
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -31,7 +39,7 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    # customer =
-    # product =
+    customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
     date_created = models.DateField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
