a0dcc5567ed9f6b0a30f052126eb84efc920d48c
nqdinh-1203 <quangdinhnt1203@gmail.com>
2022-04-21 00:32:03 +0700

    dynamic urls

--- midterm/accounts/views.py
+++ midterm/accounts/views.py
@@ -27,5 +27,12 @@ def products(request):
     return render(request, 'accounts/products.html', context)
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    
+    #total_oders = orders.count()
+    
+    context = {'customer':customer, 'orders':orders}
+    
+    return render(request, 'accounts/customer.html', context)
