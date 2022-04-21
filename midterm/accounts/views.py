04fc56b59bae630c9b7c157df6c1172230453d70
nqdinh-1203 <quangdinhnt1203@gmail.com>
2022-04-21 15:06:48 +0700

    inline formset

--- midterm/accounts/views.py
+++ midterm/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import *
 
@@ -39,16 +40,20 @@ def customer(request, pk):
     return render(request, 'accounts/customer.html', context)
 
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=3)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    
+    #form = OrderForm()
     
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
     
-    context = {'form':form}
+    context = {'formset':formset}
     return render(request, 'accounts/order/create_order.html', context)
 
 
