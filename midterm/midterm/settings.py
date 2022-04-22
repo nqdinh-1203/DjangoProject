c0d7d6e89a7f705f13ebcf1e967d926971376b1b
nqdinh-1203 <quangdinhnt1203@gmail.com>
2022-04-22 12:57:13 +0700

    password reset email

--- midterm/midterm/settings.py
+++ midterm/midterm/settings.py
@@ -133,4 +133,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dinhvaminh46@gmail.com'
+EMAIL_HOST_PASSWORD = 'dinh01694683878'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
