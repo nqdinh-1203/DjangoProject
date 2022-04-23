83c50628e676a2f7acaf783ae53007d0c9cb25aa
nqdinh-1203 <quangdinhnt1203@gmail.com>
2022-04-23 08:37:46 +0700

    final

---
+++ Readme.md
@@ -0,0 +1,1 @@
+https://github.com/nqdinh-1203/DjangoProject
--- midterm/midterm/settings.py
+++ midterm/midterm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import psycopg2
 
 
 
@@ -82,6 +83,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
+
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'PostgreSQL',
+        #'USER': 'postgres',
+        #'PASSWORD': 'dinh1203',
+        #'HOST': 'localhost',
+        #'PORT':5432
     }
 }
 
